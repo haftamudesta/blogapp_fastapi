dc085fa5da259f88c4d873479ea904f1ddad0fcc
Haftamu Desta <haftamudesta>
2026-01-28 11:13:46 -0800

    Update schemas

--- schemas.py
+++ schemas.py
@@ -9,15 +9,8 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
 
-
-class UserResponse(UserBase):
-    model_config = ConfigDict(from_attributes=True)
-
-    id: int
-    image_file: str | None
-    image_path: str
 
 class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -37,9 +30,11 @@ class UserUpdate(BaseModel):
     email: EmailStr | None = Field(default=None, max_length=120)
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -61,4 +56,4 @@ class PostResponse(PostBase):
     id: int
     user_id: int
     date_posted: datetime
-    author: UserResponse
+    author: UserPublic
